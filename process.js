f2ff263117f2a1bf14603724b7e74c7433653b1c
peerapatkulwong <80526263+peerapatkulwong@users.noreply.github.com>
2022-09-01 11:55:08 +0700

    Add files via upload

--- process.js
+++ process.js
@@ -1,13 +1,13 @@
-var button = document.getElementById('change');
-var clear = document.getElementById('clear');
+function time(){
+    var datetime = new Date()
+    var hrs = datetime.getHours();
+    var min = datetime.getMinutes();
+    var sec = datetime.getSeconds();
+    var time_unit = document.getElementById('time_unit');
 
-button.addEventListener('click',function(event){
-    var put = document.getElementById('put').value;
-    for (let i = 0; i < put.length; i++){
-        document.write(put[i] + "<br />");
-    }
-});
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
 
-clear.addEventListener('click',function(event){
-    document.getElementById('put').value = '';
-});
+setInterval(time);
